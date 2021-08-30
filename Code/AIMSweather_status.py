77c772979bfd738ce3102a045b17d278384137a6
diodon <ekleins@gmail.com>
2021-08-30 14:01:45 +1000

    add color print, last time read and parameters

--- Code/AIMSweather_status.py
+++ Code/AIMSweather_status.py
@@ -1,19 +1,35 @@
+import datetime as dt
 import json
 import requests
 import prettytable
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+END = '\033[0m'
+
 
 ## get the latest data using AIMS WS wer service
 AIMSurl = "https://api.aims.gov.au/weather/latestreadings"
 WSjson = json.loads(requests.get(AIMSurl).text)
 
 ## print the online status of each of the stations
-tbl = prettytable.PrettyTable(["ID", "STATION", "STATUS"])
+tbl = prettytable.PrettyTable(["ID", "STATION", "STATUS", "LAST READING", "PARAMETERS"])
 for item in range(len(WSjson)):
     if WSjson[item]['status']['online'] == 'true':
-        status = 'ONLINE'
+        status = BLUE + 'ONLINE' + END
     else:
-        status = 'OFFLINE'
-    tbl.add_row([WSjson[item]['site_id'], WSjson[item]['site_name'], status])
+        status = RED + 'OFFLINE' + END
+    paramList = []
+    for ss in WSjson[item]['series']:
+        if WSjson[item]['series'][ss]['status']['online'] == 'true':
+            paramList.append(BLUE + ss + END)
+        else:
+            paramList.append(RED + ss + END)
 
+    lastRead = WSjson[item]['series'][ss]['data']['date']
+    tbl.add_row([WSjson[item]['site_id'], WSjson[item]['site_name'], status, lastRead, ", ".join(paramList)])
+
+tbl.align["PARAMETERS"] = "l"
 print(tbl)
 
